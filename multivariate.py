a71efeaecf8003433c5abb5be9dc20d29377f656
PeterDamrosch <peter.damrosch@gmail.com>
2016-06-09 15:34:58 -0400

    Initial multivariate file created

---
+++ multivariate.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+# Load data on loans
+loans = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/blob/master/loans/loansData.csv')
+
+
+
