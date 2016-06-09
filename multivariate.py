598846f4729f240220dae2dd550d4b7064d45ce7
PeterDamrosch <peter.damrosch@gmail.com>
2016-06-09 18:17:21 -0400

    Tried adding numpy matrices

--- multivariate.py
+++ multivariate.py
@@ -17,12 +17,16 @@ loans['Interest.Rate'] = loans['Interest.Rate'].map(clean_interest)
 loans['Annual.Income'] = loans['Monthly.Income'] * 12
 
 # Pull out relevant series for interest rate model
-y = loans['Interest.Rate']
-x = loans['Annual.Income']
+interest_rate = loans['Interest.Rate']
+annual_income = loans['Annual.Income']
+
+# Transform series into columns for matrix
+y = np.matrix(interest_rate).transpose()
+x1 = np.matrix(annual_income).transpose()
 
 # Add a column of 1's for the model
-X = sm.add_constant(x)
+X = sm.add_constant(x1)
 
-# Create OLS model and get a summary of how well it fit
+# Create OLS model and get a summary of how well it fits
 est = sm.OLS(y,X).fit()
 print est.summary()
