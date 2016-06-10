b9d96bad919a05baca6dd43d56338054ad7b8fac
PeterDamrosch <peter.damrosch@gmail.com>
2016-06-10 11:09:26 -0400

    Model not working with linear regression. Could be not adding constant correctly

--- multivariate.py
+++ multivariate.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
 
 # Load data on loans
@@ -13,20 +14,34 @@ def clean_interest(rate):
 	return round(percent_as_decimal, 4)
 
 # Clean Interest.Rate and create Annual.Income column	 
-loans['Interest.Rate'] = loans['Interest.Rate'].map(clean_interest)
-loans['Annual.Income'] = loans['Monthly.Income'] * 12
+loans['RateClean'] = loans['Interest.Rate'].map(clean_interest)
+loans['AnnualIncome'] = loans['Monthly.Income'] * 12
+loans['Ownership'] = loans['Home.Ownership']
+
+# Visualize the relationship
+loans.plot(kind='scatter', x='AnnualIncome', y='RateClean')
+
+# Linear regression model
+lin_model = smf.ols(formula='RateClean ~ AnnualIncome', data=loans).fit()
+print lin_model.summary()
+# I'm getting an R-Squared of 0.000. AnnualIncome coeff of < 0.000001. That seems odd.
+
+# Multiple regression model
+mult_lin_model = smf.ols(formula='RateClean ~ AnnualIncome + Ownership', data=loans).fit()
+# Definitely something going on, I'm getting R2 of 0.008
+
 
 # Pull out relevant series for interest rate model
-interest_rate = loans['Interest.Rate']
-annual_income = loans['Annual.Income']
+#interest_rate = loans['Interest.Rate']
+#annual_income = loans['Annual.Income']
 
 # Transform series into columns for matrix
-y = np.matrix(interest_rate).transpose()
-x1 = np.matrix(annual_income).transpose()
+#y = np.matrix(interest_rate).transpose()
+#x1 = np.matrix(annual_income).transpose()
 
 # Add a column of 1's for the model
-X = sm.add_constant(x1)
+#X = sm.add_constant(x1)
 
 # Create OLS model and get a summary of how well it fits
-est = sm.OLS(y,X).fit()
-print est.summary()
+#est = sm.OLS(y,X).fit()
+#print est.summary()
